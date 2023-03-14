e360d595fe8f60a8694352825db8086b8705b9b1
Minh <congchuabe2000@gmail.com>
2023-03-14 14:17:07 +0700

    TH

---
+++ src/TH1_Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package TH1_Calculator;
+
+public class Calculator {
+    public static int add(int firstOperand, int secondOperand){
+//        throw new UnsupportedOperationException();
+        return firstOperand + secondOperand;
+    }
+
+    public static int sub(int firstOperand, int secondOperand){
+//        throw new UnsupportedOperationException();
+        return firstOperand - secondOperand;
+    }
+}
---
+++ src/TH2_AbsoluteNumberCalculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,9 @@
+package TH2_AbsoluteNumberCalculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findApsolute(int number){
+        if(number < 0)
+            return  -number;
+        return number;
+    }
+}
